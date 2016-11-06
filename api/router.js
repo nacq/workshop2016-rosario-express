2104621add03ff2b7a490118d4765a592e14cdaa
Nicolas Acquaviva <nicoacq@gmail.com>
2016-11-06 19:46:31 -0300

    extras jwt protected routes

--- api/router.js
+++ api/router.js
@@ -4,12 +4,30 @@ const trackController = require('./controllers/TrackController').instance; //Sam
 const bandController = require('./controllers/BandController').instance;
 const commentController = require('./controllers/CommentController').instance;
 const userController = require('./controllers/UserController').instance;
+const jwt = require('jsonwebtoken');
 
 // Specific router middleware that shows the request timestamp
 router.use((req, res, next) => {
     console.log(`${colors.green('Requesting: ')} ${req.method}  ${req.path}  -> Time: `, Date.now());
     next();
 });
+
+router.use((req, res, next) => {
+	if(req.originalUrl !== '/signin' && req.originalUrl !== '/signup') {
+		let token = req.body.token || req.query.token || req.headers['x-access-token'];
+		if(token) {
+			jwt.verify(token, 'super-complicated-secret', (err, decoded) => {
+				if(err) return res.status(401).json({ message: 'Invalid token', error: err })
+				req.decoded = decoded;
+				next()
+			})
+		} else {
+			return res.status(403).json({ message: 'No token provided' })
+		}
+	} else {
+		return next();
+	}
+})
 
 // API Routes
 router.get('/tracks', trackController.getList); //Sample route
